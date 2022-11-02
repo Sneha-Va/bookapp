c552b710a4b9e5a326066a62cafbcb5bac12ac31
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-02 13:01:19 +0530

    add

--- book.py
+++ book.py
@@ -1,3 +1,6 @@
+import mysql.connector
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='librarydb')
+mycursor = mydb.cursor()
 while True:
     print("select an option from the menu")
     print('1 add bookdetails')
@@ -10,6 +13,14 @@ while True:
     choice=int(input('enter the option:'))
     if(choice==1):
         print('add book')
+        bookname=input('enter bookname:')
+        author=input("enter author:")
+        category=input('enter category:')
+        bookchargeperday=input("enter charge:")
+        sql='INSERT INTO `book`( `bookname`, `author`, `category`, `bookchargeperday`) VALUES(%s,%s,%s,%s)'
+        data=(bookname,author,category,bookchargeperday,)
+        mycursor.execute(sql,data)
+        mydb.commit()
     if(choice==2):
         print("view book")
         
